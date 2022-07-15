6ee9a5015ec540be5d2efb3a12a9460d08598ce8
alpha <alphamodeyt@gmail.com>
2022-07-15 15:34:58 -0500

    Make harvest info stack aware

--- src/main/java/mcjty/theoneprobe/mixin/client/HumanoidArmorLayerMixin.java
+++ src/main/java/mcjty/theoneprobe/mixin/client/HumanoidArmorLayerMixin.java
@@ -1,12 +1,13 @@
 package mcjty.theoneprobe.mixin.client;
 
-import com.google.common.collect.Maps;
 import mcjty.theoneprobe.TheOneProbe;
 import net.minecraft.client.renderer.entity.layers.HumanoidArmorLayer;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ArmorItem;
-import org.jetbrains.annotations.Nullable;
-import org.spongepowered.asm.mixin.*;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
